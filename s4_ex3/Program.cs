edc1c2f443929eee691d12bf4a10c36d4b552395
ElenaShamiryan <elen91@list.ru>
2022-08-07 00:17:22 +0300

    вывод массива через запятую

---
+++ s4_ex3/Program.cs
@@ -0,0 +1,26 @@
+﻿void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int index = 0;
+    Console.Write("[");
+    while (index < count)
+    {
+        Console.Write(coll[index]);
+        index++;
+        if (index < count)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+
+Console.WriteLine("Введите числа через запятую ");
+
+int[] mass = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+Console.Write("Ваш массив: ");
+
+
+PrintArray(mass);
