4bdb658aa78e63fb2d03e8d773f51761eff6b4a7
ElenaShamiryan <elen91@list.ru>
2022-08-06 23:49:34 +0300

    сумма цифр в числе

---
+++ s4_ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumOfDigit(int n)
+{
+int sum = 0;
+while (n > 0 )
+{
+   sum = sum + n%10;
+   n = n/10;
+}
+return sum;
+}
+
+int digit = SumOfDigit(num);
+Console.WriteLine($"Сумма цифр в числе {num} равна {digit}");
